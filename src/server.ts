98cd6a9a458eab54d60964041bf39186d08bdc9b
Ramz001 <rkenjaev1@gmail.com>
2025-07-18 07:53:09 +0500

    Refactor server to implement GraphQL API with updated schema and handler. Change server port to 4000 and modify root resolver for new query structure.

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,28 @@
-import express from "express";
 import { buildSchema } from "graphql";
+import { createHandler } from "graphql-http/lib/use/express";
+import express from "express";
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`type Query { description: String, price: Float } `);
 
-
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello() {
+    return "Hello world!";
+  },
+};
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// Create and use the GraphQL handler.
+app.all(
+  "/graphql",
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
+);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// Start the server at port
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
