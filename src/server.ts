d2a613143be212015b0ecb78fe7e22c293cea9d5
Ramz001 <rkenjaev1@gmail.com>
2025-07-28 05:30:48 +0500

    Update package.json to include ruru dependency and modify server.ts to implement a new GraphQL schema with a hello query, replacing previous resolvers.

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,39 @@
-import { buildSchema } from "graphql";
-import { createHandler } from "graphql-http/lib/use/express";
 import express from "express";
-
-// Construct a schema, using GraphQL schema language
-const schema = buildSchema(`type Query { description: String, price: Float } `);
-
+import { createHandler } from "graphql-http/lib/use/express";
+import { buildSchema } from "graphql";
+// @ts-ignore
+import { ruruHTML } from "ruru/server";
+// Construct a schema using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+ 
 // The root provides a resolver function for each API endpoint
 const root = {
-  description() {
-    return "This is a description";
-  },
-  price() {
-    return 100;
+  hello() {
+    return "Hello world!";
   },
 };
-
+ 
 const app = express();
-
-// Create and use the GraphQL handler.
+ 
+// Create and use the GraphQL handler
 app.all(
   "/graphql",
   createHandler({
-    schema: schema,
+    schema,
     rootValue: root,
   })
 );
 
-// Start the server at port
-app.listen(4000);
-console.log("Running a GraphQL API server at http://localhost:4000/graphql");
+app.get("/", (_req, res) => {
+  res.type("html");
+  res.end(ruruHTML({ endpoint: "/graphql" }));
+});
+
+// Start the server at port 4000
+app.listen(4000, () => {
+  console.log("Running a GraphQL API server at http://localhost:4000/graphql");
+})
